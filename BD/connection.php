93819ac55efe244d6fcba12d6c3ea3a9938f3f20
Leonardo Dias <leonardodias914@gmail.com>
2016-08-29 17:22:40 -0300

    connection.php for connect to BD created

---
+++ BD/connection.php
@@ -0,0 +1,19 @@
+<?php
+
+	$connection;
+
+	function connect($host, $dbname, $user, $password){
+		try{
+			$connection = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $password);
+		}catch(PDOExcetion $e){
+			print "Erro: <code>" . $e->getMessage() . "</code>";
+		}
+	}
+
+	function desconnect(){
+		$connection = null;
+	}
+
+	connect("localhost", "quadrinhosfct", "root", "");
+
+?>
