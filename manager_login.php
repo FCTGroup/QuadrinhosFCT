0d8f82e6f5df57f7f6f3ba0818e98a5959af609b
Leonardo Dias <leonardodias914@gmail.com>
2016-09-21 13:46:50 -0300

    Botão voltar adicionado

--- manager_login.php
+++ manager_login.php
@@ -2,3 +2,4 @@
 <input type="text" id="txtlogin" name="txtlogin"/>
 <input type="password" id="password" name="password"/>
 <input type="button" id="login" name="login" value="login"/>
+<a href="index.php"><input type="button" value="voltar"></a>
