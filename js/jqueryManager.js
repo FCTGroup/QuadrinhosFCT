125bbda0ce79dbf24568ced56383445816fc773c
Leonardo Dias <leonardodias914@gmail.com>
2016-09-21 13:15:27 -0300

    login e logout em manager incluso

--- js/jqueryManager.js
+++ js/jqueryManager.js
@@ -10,7 +10,13 @@ $(document).ready(function(){
 			dataType:"html",
 			data:{login:login,senha:senha},
 			success:function(retorno){
-				if(retorno!="")
+				if(retorno == false){
+					$("div#manager p#message").hide();
+					$("div#manager p#message").html('login ou senha inválidos');
+					$("div#manager p#message").css("color","#FF0000");
+					$("div#manager p#message").fadeIn(1000);
+				}
+				else
 					$("div#manager").html(retorno);
 			}
 		});
