0ca98962591123ac44e79da54ff4c9e9f827b6ee
Leonardo Dias <leonardodias914@gmail.com>
2016-09-21 13:43:31 -0300

    conteúdo manager adicionado

--- manager_conteudo.php
+++ manager_conteudo.php
@@ -1,4 +1,15 @@
 <?php
 	$login = $_SESSION["login"];
-	echo '<p>'.$login.' <input type="button" value="logout" id="logout"/></p>';
+	echo '<p>'.$login.' <input type="button" value="logout" id="logout"/>';
+	echo '<a href="index.php"><input type="button" value="voltar"></a></p>';
+
 ?>
+
+	<a href="formsListagem/categoria.php"><p>Categoria</p></a>
+	<a href="formsListagem/editora.php"><p>Editora</p></a>
+	<a href="formsListagem/formato.php"><p>Formato</p></a>
+	<a href="formsListagem/genero.php"><p>Genero</p></a>
+	<a href="formsListagem/licenciador.php"><p>Licenciador</p></a>
+	<a href="formsListagem/status.php"><p>Status</p></a>
+	<a href="formsListagem/quadrinho.php"><p>Quadrinho</p></a>
+	<a href="formsListagem/usuario.php"><p>Usuário</p></a>
--- manager_login.php
+++ manager_login.php
@@ -1,3 +1,4 @@
+<p id="message"></p>
 <input type="text" id="txtlogin" name="txtlogin"/>
 <input type="password" id="password" name="password"/>
 <input type="button" id="login" name="login" value="login"/>
